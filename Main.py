8ad587f0ce83b979ee5a0c55f3dbbf11270865b1
vision011 <MOHA2048@umn.edu>
2025-01-08 00:28:10 -0600

    Functionality changes
    Added a way for a user to decide not to play the game

--- Main.py
+++ Main.py
@@ -7,15 +7,15 @@ Pillar_interface.display_pillar3_intro()
 
 
 first_name = input("\n         Please enter your First Name: ") 
-last_name = input("\n          Please enter your Last Name: ")
+last_name = input("\n         Please enter your Last Name: ")
 
 data_base = {}
 
 name = first_name + " " + last_name
 
 if name not in data_base:
-    print("It seems you have not created and account yet!")
-    password = input("Please enter your password: ")
+    print("\n         It seems you have not created and account yet!")
+    password = input("\n         Please enter your password: ")
     user = User(first_name,last_name,password)
     user.hash_password
 
@@ -28,16 +28,23 @@ if name not in data_base:
 
 
 
+user_choice = input("\n         Would you like to try and guess today's word?")
+
+
+Game = WordGame()
+
+if user_choice == "Y":
+    Game.play_game()
+else:
+    Pillar_interface.clear_terminal()
+    print("No worries sometimes we like to just get into it as well!")
+    charity, word = Game.get_charity_and_word_of_the_day()
+    print(Game.colorize_string(f"Today's Charity is: {charity}", "\033[33m"))
+    print(Game.colorize_string(f"Learn more and support them here: {Game.charity_links[charity]}\n", "\033[36m"))
 
 
 
-Game = WordGame() 
 
 
 
 
-
-
-
-
-Game.play_game(name)
